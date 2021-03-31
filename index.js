293d20c308845f72a8a4b0000f227b9502d141dc
Shahed70 <58668503+Shahed70@users.noreply.github.com>
2021-04-01 03:33:33 +0600

    test commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const { urlencoded } = require('express');
+const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT || 5000;
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.em8kw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
@@ -18,8 +19,37 @@ app.get('/',(req, res)=>{
 })
 
 client.connect(err => {
-    const collection = client.db("productsdb").collection("products");
+    const productCollection = client.db("productsdb").collection("products");
+    app.post('/addProduct', (req, res)=>{
+      const product = req.body
+      productCollection.insertOne(product)
+      .then(result => {
+        res.send(result.insertedCount > 0)
+      })
+
+    })
+
+    app.get('/getProduct', (req, res)=>{
+      productCollection.find({})
+      .toArray((err, docs)=>{
+        res.send(docs)
+      })
+    })
+
+    app.get('/getOne/:id', (req, res)=>{
+      productCollection.deleteOne({_id:ObjectId(req.params.id)})
+      .then(result =>{
+          res.send(result)
+      })
+    })
+
+    app.get('/deleteProduct', (req, res)=>{
+      productCollection.deleteOne({_id:ObjectId(req.params.id)})
+      .then(result =>{
+          res.send(result)
+      })
+    })
     
   });
 
-app.listen(port, ()=> console.log('connected'))
+app.listen(port, ()=> console.log('Server is listening successfully on ' + port))
