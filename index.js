087c1f35c7477656a98e8cbb3d39f0a0300f538e
Shahed70 <58668503+Shahed70@users.noreply.github.com>
2021-04-02 02:15:48 +0600

    fix some issue

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.em8kw.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
 const app = express()
 app.use(express.json())
 app.use(urlencoded({extended:false}))
